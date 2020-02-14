6660ac80b1e09ba4ca982ff153631312137772de
Zachary <zpyingling9559@gmail.com>
2020-02-13 19:53:39 -0500

    adding display on search to work; getting tour dates to work and getting the artist image to work

--- assets/logic.js
+++ assets/logic.js
@@ -7,51 +7,67 @@ $("#submit-button").on("click", function(e) {
     "https://rest.bandsintown.com/artists/" +
     artist +
     "/events?app_id=codingbootcamp";
+
   $.ajax({
     url: queryURL,
     method: "GET"
   }).then(function(response) {
-    // Useless code now; i just threw some stuff up when we had to present
-    //
-    // $("#information").empty();
-    // console.log(response[0].artist);
-    // let artistName = response[0].artist.name;
-    // let header = $("<h1>");
-    // header.text(artistName);
-    // $("#information").prepend(header);
-    // let artistImage = $("<img>");
-    // artistImage.attr("src", response[0].artist.image_url);
-    // artistImage.attr("alt", "Artist image");
-    // artistImage.attr("width", "400");
-    // artistImage.attr("height", "400");
-    // $("#information").append(artistImage);
-    // let anchorTag = $("<a>");
-    // anchorTag.text("Visit the artist's facebook page");
-    // anchorTag.attr("href", response[0].artist.facebook_page_url);
-    // anchorTag.attr("target", "_blank");
-    // $("#information").append("<br>");
-    // $("#information").append(anchorTag);
+    console.log(response);
+    if (artist === "" || response.length === 0) {
+      // Modal
+    } else {
+      $(".display-none").attr("class", "display-all");
+      let numEvents = response.length;
+      // Putting artist info from bands in town on page
+      $("#img").attr("src", response[0].artist.image_url);
+
+      // Adding all of the artists tour dates to the table
+      for (let i = 0; i < numEvents; i++) {
+        let newRow = $("<tr>");
+        let newNumber = $("<th>");
+        newNumber.text(i + 1);
+        let newDate = $("<td>");
+        newDate.text(response[i].datetime);
+        let newVenue = $("<td>");
+        newVenue.text(response[i].venue.name);
+        let newLocation = $("<td>");
+        newLocation.text(
+          response[i].venue.city + ", " + response[i].venue.region
+        );
+        let newTickets = $("<td>");
+        let ticketsAnchor = $("<a>");
+        ticketsAnchor.attr("href", response[i].url);
+        ticketsAnchor.text("Tickets");
+        ticketsAnchor.attr("target", "_blank");
+        newTickets.append(ticketsAnchor);
+
+        newRow.append(newNumber);
+        newRow.append(newDate);
+        newRow.append(newVenue);
+        newRow.append(newLocation);
+        newRow.append(newTickets);
+        $("#tour-dates").append(newRow);
+      }
+      youtubeCall();
+    }
   });
 });
 
 // Youtube functionality
-$(function() {
-  $("form").on("submit", function(e) {
-    e.preventDefault();
-    //prepare the request
-    var request = gapi.client.youtube.search.list({
-      part: "snippet",
-      type: "video",
-      q: encodeURIComponent($("#search").val()).replace(/%20/g, "+"),
-      maxResults: 5,
-      order: "viewCount"
-    });
-    //execute request
-    request.execute(function(response) {
-      console.log(response);
-    });
+function youtubeCall() {
+  //prepare the request
+  var request = gapi.client.youtube.search.list({
+    part: "snippet",
+    type: "video",
+    q: encodeURIComponent($("#search").val()).replace(/%20/g, "+"),
+    maxResults: 5,
+    order: "viewCount"
   });
-});
+  //execute request
+  request.execute(function(response) {
+    console.log(response);
+  });
+}
 
 function init() {
   gapi.client.setApiKey("AIzaSyApyK0pdQLdwW4lBRGeKowZn5DG1h4l7Do");
